cf9ede628a42423349c8df2d86a5f339fd95465d
Freddie Skrine <ff23@ic.ac.uk>
2025-04-21 16:08:53 +0100

    commit

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,8 +1,15 @@
+from numbers import Integral
+
 def fib(n):
     """Return the n-th Fibonacci number."""
+    if not isinstance(n, Integral):
+        raise TypeError(
+            "fib expects an integer."
+        )
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
         return fib(n-2) + fib(n-1)
+
---
+++ test.py
@@ -0,0 +1,5 @@
+from example_code.polynomial import Polynomial  # NOQA D100
+
+p = Polynomial((1, 1))
+
+print(1+p)
